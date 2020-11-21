fe4b7ff8ba3e32b3e8dea9e5ee8c8bc6ece30fda
Chris DeBonte <christopher.debonte@gmail.com>
2020-11-21 14:29:54 -0500

    Collision complete for cars
    Changed background to black on images because it looks better. Collision is implemented. collision function implmented. -CD

--- main.cpp
+++ main.cpp
@@ -22,6 +22,7 @@ struct carSprite
 void moveCharacter(sprite &myS);
 void drawCharacter(sprite &myS, BITMAP *b, BITMAP *buffer);
 void drawCars(vector<carSprite> &carArray, BITMAP *cars[][21], BITMAP *buf, int speed);
+void collision(vector<carSprite> &carArray, sprite &myS, int &score);
 
 int main() {
 	init();
@@ -30,12 +31,12 @@ int main() {
 	int startY = 550;
 	
 	BITMAP *playerSprite;
-	playerSprite = load_bitmap("spriteSmall.bmp", NULL);
+	playerSprite = load_bitmap("spriteSmallBlackBackground.bmp", NULL);
 	
 	BITMAP *car_spritesheet;
      BITMAP *cars[8][21];     
 	//load the car bitmap
-	car_spritesheet = load_bitmap("Car Wars Counters A 24jun2013____GTA_1.bmp", NULL);
+	car_spritesheet = load_bitmap("Car Wars Counters A 24jun2013____GTA_2.bmp", NULL);
 	//placeholder to blit the cars into
 	BITMAP *placeholder;
 	placeholder = create_bitmap(100, 50);
@@ -63,28 +64,55 @@ int main() {
 	
     }
     
+    //create vectors of cars for the first row of cars
     vector<carSprite> firstRow;
-    carSprite csp = {0,0,0,0};
+    carSprite csp = {-100,500,rand() % 8,rand() % 21};
     firstRow.push_back(csp);
-    //firstRow.push_back({100, 500, 7 ,1});
-     
+    carSprite csp1 = {200,500,rand() % 8,rand() % 21};
+    firstRow.push_back(csp1);
+    carSprite csp2 = {500,500,rand() % 8,rand() % 21};
+    firstRow.push_back(csp2);
+    carSprite csp3 = {800,500,rand() % 8,rand() % 21};
+    firstRow.push_back(csp3);
     
+    
+     vector<carSprite> thirdRow;
+    carSprite csp4 = {0,300,rand() % 8,rand() % 21};
+    thirdRow.push_back(csp4);
+    carSprite csp5 = {200,300,rand() % 8,rand() % 21};
+    thirdRow.push_back(csp5);
+    carSprite csp6 = {500,300,rand() % 8,rand() % 21};
+    thirdRow.push_back(csp6);
+    
+    vector<carSprite> fifthRow;
+    carSprite csp7 = {12,350,rand() % 8,rand() % 21};
+    fifthRow.push_back(csp7);
+    carSprite csp8 = {225,350,rand() % 8,rand() % 21};
+    fifthRow.push_back(csp8);
+    carSprite csp9 = {476,350,rand() % 8,rand() % 21};
+    fifthRow.push_back(csp9);
+    carSprite csp10 = {789,350,rand() % 8,rand() % 21};
+    fifthRow.push_back(csp10);
 	while (!key[KEY_ESC]) {
 		rectfill(buffer, 0, 0, 800, 600, BLACK);
 		rectfill(buffer, 0, 550, 800, 600, makecol(255,0,255));
 		moveCharacter(player);
 		drawCharacter(player, playerSprite, buffer);
 		drawCars(firstRow, cars, buffer, 20);
-		
+		drawCars(thirdRow, cars, buffer, 10);
+		drawCars(fifthRow, cars, buffer, 7);
+		collision(firstRow, player, lives);
+		collision(thirdRow, player, lives);
+		collision(fifthRow, player, lives);
 		
 		
 		
 	      // write amount of lives on screen	
-          //textprintf(buffer, font, 0, 10, WHITE, "Lives %i", lives);
+          textprintf(buffer, font, 0, 10, WHITE, "Lives %i", lives);
 	
 	      //copy the buffer to the screen
 	      blit(buffer, screen, 0,0,0,0,800,600);
-	      rest(50);
+	      rest(75);
 	}
 
 
@@ -108,6 +136,28 @@ int main() {
 	return 0;
 }
 END_OF_MAIN()
+
+void collision(vector<carSprite> &carArray, sprite &myS, int &score)
+{
+     if(myS.yLoc == carArray.at(0).yLoc)
+     {
+                 for(int i = 0; i < carArray.size(); i++)
+                 {
+                         if(myS.xLoc > carArray.at(i).xLoc && myS.xLoc < carArray.at(i).xLoc +75)
+                         {
+                                 myS.xLoc = 375;
+                                 myS.yLoc = 550;  
+                                 score -=1;  
+                                 return;
+                         }
+                 
+                 
+                 }
+     
+     
+     
+     }
+}
 void drawCars(vector<carSprite> &carArray, BITMAP *cars[][21], BITMAP *buf, int speed)
 {
      
@@ -116,13 +166,13 @@ void drawCars(vector<carSprite> &carArray, BITMAP *cars[][21], BITMAP *buf, int 
              
              
              draw_sprite(buf, cars[carArray.at(i).carX][carArray.at(i).carY], carArray.at(i).xLoc, carArray[i].yLoc);
-		     carArray.at(i).xLoc += 20;
+		     carArray.at(i).xLoc += speed;
 		     
-           if(carArray.at(i).xLoc > 800 && speed > 0)
-             {carArray.at(i).xLoc = 0;}
+           if(carArray.at(i).xLoc > 900 && speed > 0)
+             {carArray.at(i).xLoc = -100;}
      
-             if(carArray.at(i).xLoc < 0 && speed < 0)
-             {carArray.at(i).xLoc = 800;}
+             if(carArray.at(i).xLoc < -100 && speed < 0)
+             {carArray.at(i).xLoc = 900;}
      
      
              }
