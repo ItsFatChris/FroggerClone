b7a9f0302a67a2e33ab2b54d84135332783a4474
Chris DeBonte <christopher.debonte@gmail.com>
2020-11-21 10:48:22 -0500

    Draws cars and moves them
    Splits spritesheet and now will draw rows of cars and adjust their speed and reset them when they go off screen. drawCars function. -CD

--- main.cpp
+++ main.cpp
@@ -1,15 +1,27 @@
 #include <allegro.h>
-
+#include <iterator>
+#include <vector>
+#define BLACK makecol(0,0,0)
+#define WHITE makecol(255,255,255)
 void init();
 void deinit();
-
+using namespace std;
 struct sprite
 {
        int xLoc;
        int yLoc;
        };
+       
+struct carSprite
+{
+       int xLoc;
+       int yLoc;
+       int carX;
+       int carY;
+       };
 void moveCharacter(sprite &myS);
 void drawCharacter(sprite &myS, BITMAP *b, BITMAP *buffer);
+void drawCars(vector<carSprite> &carArray, BITMAP *cars[][21], BITMAP *buf, int speed);
 
 int main() {
 	init();
@@ -20,49 +32,121 @@ int main() {
 	BITMAP *playerSprite;
 	playerSprite = load_bitmap("spriteSmall.bmp", NULL);
 	
+	BITMAP *car_spritesheet;
+     BITMAP *cars[8][21];     
+	//load the car bitmap
+	car_spritesheet = load_bitmap("Car Wars Counters A 24jun2013____GTA_1.bmp", NULL);
+	//placeholder to blit the cars into
+	BITMAP *placeholder;
+	placeholder = create_bitmap(100, 50);
+	
     BITMAP *buffer =NULL; //create buffer
     buffer = create_bitmap(800, 600);
     sprite player = {startX, startY};
     
+    //split spritesheet into the array
+    for(int x=0; x<8; x++)
+	{
+	         for(int y=0; y<21; y++)
+	         {        
+                      //needs a bitmap to copy to, so load a blank placeholder bitmap
+                      cars[x][y] = load_bitmap("placeholder.bmp", NULL); //put the location of your file here
+                      blit(car_spritesheet, //bitmap source
+                      cars[x][y],           // bitmap destination
+                      x*128 +14, y*64+7,              // source  x and y
+                      0,0,                   // destination xand y
+                      100, 50);              // width and height
+                      
+                      }
+	
+	
+	
+    }
+    
+    vector<carSprite> firstRow;
+    carSprite csp = {0,0,0,0};
+    firstRow.push_back(csp);
+    //firstRow.push_back({100, 500, 7 ,1});
+     
     
 	while (!key[KEY_ESC]) {
-		rectfill(buffer, 0, 0, 800, 600, makecol(0,0,0));
+		rectfill(buffer, 0, 0, 800, 600, BLACK);
 		rectfill(buffer, 0, 550, 800, 600, makecol(255,0,255));
 		moveCharacter(player);
-		//drawCharacter(player, playerSprite, buffer);
-		draw_sprite(buffer, playerSprite, player.xLoc, player.yLoc);
+		drawCharacter(player, playerSprite, buffer);
+		drawCars(firstRow, cars, buffer, 20);
 		
 		
 		
 		
-		
-	blit(buffer, screen, 0,0,0,0,800,600);
-	rest(100);
+	      // write amount of lives on screen	
+          //textprintf(buffer, font, 0, 10, WHITE, "Lives %i", lives);
+	
+	      //copy the buffer to the screen
+	      blit(buffer, screen, 0,0,0,0,800,600);
+	      rest(50);
 	}
+
+
+     //destroy bitmaps in memory
+    destroy_bitmap(car_spritesheet);
+    destroy_bitmap(playerSprite);
+    destroy_bitmap(buffer);
+   for(int x=0; x<8; x++)
+	{
+	         for(int y=0; y<21; y++)
+	         {
+                      destroy_bitmap(cars[x][y] );
+                      
+                      }
+	
+	
+	
+    }
 
 	deinit();
 	return 0;
 }
 END_OF_MAIN()
-
+void drawCars(vector<carSprite> &carArray, BITMAP *cars[][21], BITMAP *buf, int speed)
+{
+     
+     for(int i = 0; i < carArray.size(); i++)
+     {
+             
+             
+             draw_sprite(buf, cars[carArray.at(i).carX][carArray.at(i).carY], carArray.at(i).xLoc, carArray[i].yLoc);
+		     carArray.at(i).xLoc += 20;
+		     
+           if(carArray.at(i).xLoc > 800 && speed > 0)
+             {carArray.at(i).xLoc = 0;}
+     
+             if(carArray.at(i).xLoc < 0 && speed < 0)
+             {carArray.at(i).xLoc = 800;}
+     
+     
+             }
+     
+     
+     }
 void moveCharacter(sprite &myS)
 {
      
       if (key[KEY_LEFT]) { 
           if(myS.xLoc > 0)
           {
-          myS.xLoc -= 1; 
+          myS.xLoc -= 20; 
           }             
      }
         if (key[KEY_RIGHT]) { 
-          if(myS.xLoc < 800)
+          if(myS.xLoc+50 < 800)
           {
-          myS.xLoc += 1; 
+          myS.xLoc += 20; 
           }    
                         // textprintf(screen, font, 0, 10, WHITE, "XLocation: %i", myS.xOld);
      }
         if (key[KEY_DOWN]) { 
-        if(myS.yLoc+49 < 600)
+        if(myS.yLoc+50 < 600)
           {
           myS.yLoc += 50; 
           }  
@@ -83,7 +167,7 @@ void drawCharacter(sprite &myS, BITMAP *b, BITMAP *buffer)
 {
      
      //draw the sprite
-     draw_sprite(buffer, b, myS.xLoc, myS.yLoc*50);
+     draw_sprite(buffer, b, myS.xLoc, myS.yLoc);
      
      
      }
